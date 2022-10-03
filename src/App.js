fa7122c8d3187c3fc6f9a4d8f6454f6b5f3e7a1e
unknown <ugurcan.uut82@gmail.com>
2022-10-03 21:22:02 +0300

    contact landlord, mailto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
+import Contact from './pages/Contact';
 import CreateListing from './pages/CreateListing';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
@@ -28,6 +29,7 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
